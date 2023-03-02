a9ea28518bb5ababe4183430dfc0b93ed77ee370
maooson <maooson@qq.com>
2023-03-03 00:23:14 +0800

    Add google ads.txt

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import postcss from './postcss.config.js'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +11,17 @@ export default defineConfig({
   css: {
     postcss,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: './ads.txt',
+          dest: ''
+        }
+      ]
+    })
+  ],
   resolve: {
     alias: [
       {
@@ -25,5 +36,5 @@ export default defineConfig({
     commonjsOptions: {
       transformMixedEsModules: true,
     }
-  } 
+  }
 })
