46fa26b87dd3daf206d2c34b58cba2e26175b4f6
clovelee <w.shangli@icloud.com>
2023-03-15 16:28:54 +0800

    add gpt4 模型

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -44,13 +44,13 @@ function Header() {
           <nav className="flex flex-grow">
             <ul className="flex flex-grow justify-end flex-wrap items-center">
               <li>
-                <a href="https://chatgpt4filehelper.aow.me" className="font-medium text-gray-600 hover:text-gray-900 py-3 flex items-center transition duration-150 ease-in-out">个人版</a>
+                <a href="https://chatgpt4filehelper.aow.me" className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">个人助手版</a>
+              </li>
+              <li>
+                <a target="_blank" href="https://as-notion-git-main-weilai.vercel.app/blog" className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">GPT 使用指南</a>
               </li>
               <li>
                 <a href="https://chat.aoq.me" className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out">加入社区</a>
-              </li>
-              <li>
-                <a target="_blank" href="https://weilaimeixue.notion.site/weilaimeixue/As-3bc4631f854e44d78825d4a4e73b2e02" className="font-medium text-gray-600 hover:text-gray-900 py-3 flex items-center transition duration-150 ease-in-out">了解更多</a>
               </li>
             </ul>
           </nav>
--- src/partials/Pricing.jsx
+++ src/partials/Pricing.jsx
@@ -9,10 +9,13 @@ export default function PricingTable() {
                         <div class="p-8 text-2xl font-bold text-center">Free <br /><span class="text-sm">(仅限个人微信群使用)</span></div>
                         <div class="border-0 border-grey-light border-t border-solid text-sm">
                             <div class="text-center border-0 border-grey-light border-b border-solid py-3">
-                                支持ChatGPT 网页版
+                                支持ChatGPT 网页端
                             </div>
                             <div class="text-center border-0 border-grey-light border-b border-solid py-3">
-                                支持ChatGPT API
+                                ChatGPT 网页端模型可选（已支持4.0）
+                            </div>
+                            <div class="text-center border-0 border-grey-light border-b border-solid py-3">
+                                支持ChatGPT API gpt4 模型
                             </div>
                             <div class="text-center border-0 border-grey-light border-b border-solid py-3">
                                 API支持上下文
@@ -20,21 +23,12 @@ export default function PricingTable() {
                         </div>
                         <div class="w-full text-center px-8 pt-8 pb-4 text-xl mt-auto">
                         </div>
-                        <div class="text-center mt-8 mb-8">
-                            <a href="https://aow.me/offline_latest" class="inline-block bg-white border border-gray-300 text-black-500 px-6 py-3 rounded-lg hover:bg-blue-500 hover:text-white hover:no-underline">离线包下载</a>
-                        </div>
                     </div>
                     <div class="flex-1 lg:flex-initial lg:w-1/4 rounded-lg hover:bg-blue-100  hover:text-blue-500 bg-white mt-4 sm:-mt-4 shadow-lg z-30 flex flex-col">
                         <div class="w-full p-8 text-2xl font-bold text-center">Plus<br /><span class="text-sm">(仅限个人微信群使用)</span></div>
                         <div class="w-full border-0 border-grey-light border-t border-solid text-sm">
                             <div class="text-center border-0 border-grey-light border-b border-solid py-3">
-                                支持ChatGPT 网页版
-                            </div>
-                            <div class="text-center border-0 border-grey-light border-b border-solid py-3">
-                                支持ChatGPT API
-                            </div>
-                            <div class="text-center border-0 border-grey-light border-b border-solid py-3">
-                                API支持上下文
+                                包含免费版所有功能
                             </div>
                             <div class="text-center border-0 border-grey-light border-b border-solid py-3">
                                 🌟 定义AI身份/角色
@@ -45,25 +39,28 @@ export default function PricingTable() {
                             <div class="text-center border-0 border-grey-light border-b border-solid py-3">
                                 🌟 自定义队列积压告警阈值
                             </div>
+                            <div class="text-center border-0 border-grey-light border-b border-solid py-3">
+                                🌟 一键开启 Bot 接管
+                            </div>
+                            <div class="text-center border-0 border-grey-light border-b border-solid py-3">
+                                🌟 入群欢迎语
+                            </div>
                         </div>
                         <div class="w-full text-center px-8 pt-8 pb-4 text-xl mt-auto">
-                            ¥ 50
-                        </div>
-                        <div class="w-full text-center mb-8 mt-auto">
-                            <a href="https://aow.me/offline_plus" class="inline-block bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-500 hover:text-white hover:no-underline">离线包下载</a>
+                            ¥ 100
                         </div>
                         <div class="w-full text-center px-8 pb-8 mt-auto text-gray-500 text-sm">
-                            购买 Plus 版本，请添加微信：Asgrief，获取下载密钥。
+                            购买升级密钥，请添加微信：Asgrief。
                         </div>
                     </div>
                     <div class="flex-1 lg:flex-initial lg:w-1/4 rounded-lg hover:bg-blue-100  hover:text-blue-500 bg-white mt-4 mr-12 ml-12 flex flex-col">
                         <div class="p-8 text-2xl font-bold text-center">商业版<br /><span class="text-sm">(可用于付费或企业微信群)</span></div>
                         <div class="border-0 border-grey-light border-t border-solid text-sm">
                             <div class="text-center border-0 border-grey-light border-b border-solid py-3">
-                                商业授权
+                                含 Plus 版本所有功能
                             </div>
                             <div class="text-center border-0 border-grey-light border-b border-solid py-3">
-                                含所有Plus版本所有功能
+                                🌟 商业授权
                             </div>
                             <div class="text-center border-0 border-grey-light border-b border-solid py-3">
                                 过滤敏感词API
@@ -75,11 +72,8 @@ export default function PricingTable() {
                                 定制其他需求
                             </div>
                         </div>
-                        <div class="text-center px-8 pt-8 text-xl mt-auto">
+                        <div class="w-full text-center px-8 pt-8 pb-8 text-xl mt-auto">
                             联系咨询
-                        </div>
-                        <div class="text-center pt-8 mb-8 mt-auto">
-                            <a href="#" class="inline-block bg-gray-300 text-white px-6 py-3 rounded-lg hover:text-white hover:no-underline">暂未开放</a>
                         </div>
                     </div>
                 </div>
